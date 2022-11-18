f68d796c500ee52417e5b567f3c8c607c1908f53
Tom Rini <trini@konsulko.com>
2022-11-18 13:39:37 -0500

    global: Migrate CONFIG_SANDBOX_ARCH to CFG
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- arch/sandbox/include/asm/config.h
+++ arch/sandbox/include/asm/config.h
@@ -6,7 +6,7 @@
 #ifndef _ASM_CONFIG_H_
 #define _ASM_CONFIG_H_
 
-#define CONFIG_SANDBOX_ARCH
+#define CFG_SANDBOX_ARCH
 
 /* Used by drivers/spi/sandbox_spi.c and arch/sandbox/include/asm/state.h */
 #ifndef CONFIG_SANDBOX_SPI_MAX_BUS
