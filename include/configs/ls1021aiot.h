93f00f5de4df18163ecc430a5dd035ff57b882d2
Tom Rini <trini@konsulko.com>
2022-11-18 13:39:46 -0500

    global: Migrate CONFIG_SMP_PEN_ADDR to CFG
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/configs/ls1021aiot.h
+++ include/configs/ls1021aiot.h
@@ -90,7 +90,7 @@
 #define FSL_PCIE_COMPAT		"fsl,ls1021a-pcie"
 
 #define CFG_PEN_ADDR_BIG_ENDIAN
-#define CONFIG_SMP_PEN_ADDR		0x01ee0200
+#define CFG_SMP_PEN_ADDR		0x01ee0200
 
 #define HWCONFIG_BUFFER_SIZE		256
 
