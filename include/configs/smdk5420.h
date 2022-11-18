733494f2ec425fbcb403087b8c8e522530896a87
Tom Rini <trini@konsulko.com>
2022-11-18 13:39:45 -0500

    global: Migrate CONFIG_SMDK5420 to CFG
    Signed-off-by: Tom Rini <trini@konsulko.com>

--- include/configs/smdk5420.h
+++ include/configs/smdk5420.h
@@ -5,18 +5,18 @@
  * Configuration settings for the SAMSUNG SMDK5420 board.
  */
 
-#ifndef __CONFIG_SMDK5420_H
-#define __CONFIG_SMDK5420_H
+#ifndef __CFG_SMDK5420_H
+#define __CFG_SMDK5420_H
 
 #include <configs/exynos5420-common.h>
 #include <configs/exynos5-dt-common.h>
 #include <configs/exynos5-common.h>
 
-#define CONFIG_SMDK5420			/* which is in a SMDK5420 */
+#define CFG_SMDK5420			/* which is in a SMDK5420 */
 
 #define CFG_SYS_SDRAM_BASE	0x20000000
 
 /* DRAM Memory Banks */
 #define SDRAM_BANK_SIZE		(512UL << 20UL)	/* 512 MB */
 
-#endif	/* __CONFIG_SMDK5420_H */
+#endif	/* __CFG_SMDK5420_H */
